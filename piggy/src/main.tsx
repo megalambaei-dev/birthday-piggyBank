f7a2c05efb944b043eb41ab5e75a8c5825c844a9
MEGalamba <mariaeduardagalamba@gmail.com>
2026-09-10 14:30:49 +0100

    Added page to main and installed router dom

--- piggy/src/main.tsx
+++ piggy/src/main.tsx
@@ -1,5 +1,14 @@
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
+import PaginaPersonalizar from './pages/PaginaPersonalizar'
 import React from 'react'
 
-createRoot(document.getElementById('app')!).render(<App />)
+createRoot(document.getElementById('app')!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/personalizar" element={<PaginaPersonalizar />} />
+    </Routes>
+  </BrowserRouter>
+)
